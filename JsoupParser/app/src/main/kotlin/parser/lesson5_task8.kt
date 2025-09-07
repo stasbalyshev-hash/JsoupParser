2c8de1e27183eea122da0a750f898fffc47214af
Stas <stas.balyshev@mail.ru>
2025-09-07 13:12:12 +0300

    KS-5-8 parser

--- JsoupParser/app/src/main/kotlin/parser/lesson5_task8.kt
+++ JsoupParser/app/src/main/kotlin/parser/lesson5_task8.kt
@@ -1,2 +1,19 @@
-package org.example.app.parser
+package org.example.app
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
+fun main() {
+
+    val doc: Document =
+        Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
+            .get()
+    val quotes: Elements = doc.select("article.sc-14uz67c-0")
+
+    for (textOfQuotes: Element in quotes) {
+        val text = textOfQuotes.text()
+        println(text)
+    }
+}
